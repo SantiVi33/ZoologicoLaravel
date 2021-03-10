c625aad7a126367ae92a2b1d7e22d6c96737b658
SantiVi33 <santi.vinuela@hotmail.com>
2021-03-10 11:28:46 -0300

    actualizacion del controlador API para visualizarlas

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Animal;
+use App\Models\Persona;
 
-class ApiController extends Controller
+class ApiController extends Controller 
 {
-    //
+    //public function AgregarAnimal(request $request)
+    //{
+        
+       // $animal = Animal::create($request->all()); 
+      //  return $animal;
+
+    //}
+
+    
+    public function VerAnimal()
+    {
+
+        $animal = Animal::all();
+        return $animal;
+    }
+
+    //public function AgregarPersona(request $request)
+    //{
+   
+        //$persona = Persona::create($request->all());
+      //  return $persona;
+
+    //}
+
+    
+    public function VerPersonal()
+    {
+
+        $persona = Persona::all();
+        return $persona;
+    }
 }
