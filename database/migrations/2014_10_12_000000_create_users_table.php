bf5131adb631f7f8d44c93d8aed4bad128a7b66b
SantiVi33 <santi.vinuela@hotmail.com>
2021-03-15 15:49:07 -0300

    Configuracion tabla Usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('Nombre');
+            $table->string('Apellido');
+            $table->string('TipDocumento');
+            $table->number('NroDocumento');
+            $table->string('Email')->unique();
+            $table->string('Usuario');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
